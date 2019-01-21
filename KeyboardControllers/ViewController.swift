2724955cca36ec88b09432f0976d2cbc41854bf0
Hector Mendoza <hectormendoza@byui.edu>
2019-01-21 16:29:12 -0500

    Basic keyboard functions implemented

--- KeyboardControllers/ViewController.swift
+++ KeyboardControllers/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var keyboardHeight: CGFloat = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        addKeyboardNotifications()
     }
+    
+}
 
-
+//MARK: - Keyboard Control Methods
+extension ViewController {
+    
+    func addKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else {return}
+        
+        guard let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {return}
+        
+        let keyboardFrame = keyboardSize.cgRectValue
+        keyboardHeight = keyboardFrame.height
+        
+        view.frame.size.height -= keyboardHeight
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        view.frame.size.height += keyboardHeight
+    }
+    
+    //We hide the keyboard when touched outside textfield
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
